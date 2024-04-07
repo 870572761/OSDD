e1e56dc37ed3addbefcbe291da983ec378b20a82
leihaodong <870572761@qq.com>
2024-04-07 14:29:44 +0800

    一些小bug的修改

--- lib/test/evaluation/__init__.py
+++ lib/test/evaluation/__init__.py
@@ -1,4 +1,5 @@
 from .data import Sequence
 from .tracker import Tracker, trackerlist
+from .tracker1 import Tracker1
 from .datasets import get_dataset
 from .environment import create_default_local_file_ITP_test
